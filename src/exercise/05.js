91345e0aadee035c51ff3d4af32e309ce19dd8dc
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-01-19 08:01:09 +0200

    State Reducer-extra credit

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -55,25 +55,21 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
   }
 }
 
+// export { useToggle, toggleReducer }
+
+// ---
+
+// import { useToggle, toggleReducer }
+
 function App() {
   const [timesClicked, setTimesClicked] = React.useState(0)
   const clickedTooMuch = timesClicked >= 4
 
   function toggleStateReducer(state, action) {
-    switch (action.type) {
-      case 'toggle': {
-        if (clickedTooMuch) {
-          return {on: state.on}
-        }
-        return {on: !state.on}
-      }
-      case 'reset': {
-        return {on: false}
-      }
-      default: {
-        throw new Error(`Unsupported type: ${action.type}`)
-      }
+    if (action.type === 'toggle' && timesClicked >= 4) {
+      return {on: state.on}
     }
+    return toggleReducer(state, action)
   }
 
   const {on, getTogglerProps, getResetterProps} = useToggle({
@@ -105,8 +101,3 @@ function App() {
 }
 
 export default App
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
